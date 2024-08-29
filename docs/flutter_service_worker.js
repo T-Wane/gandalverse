598a4436fd120339b2e2a5c83e149801b0e98cd9
Jbriguel <dossouyovojoseph04@gmail.com>
2024-08-29 20:26:52 +0000

    update - new

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -137,9 +137,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "9e2c9c65a0694e121db37c4e8e8515e9
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "a6a03b142e5bfd03221c3a751787314a",
-"/": "a6a03b142e5bfd03221c3a751787314a",
-"main.dart.js": "d0504b8bbe59d6604250cc891e51daca",
+"index.html": "663bb774dac7a4a3d63d9cbda8ef187c",
+"/": "663bb774dac7a4a3d63d9cbda8ef187c",
+"main.dart.js": "f31d43d9f8cca130cb31794140ffb961",
 "manifest.json": "9f6f1bf5b67cd2b30e68a99551f02cad",
 "version.json": "bdbf5533cd872db8379bf13ff58db6f3"};
 // The application shell files that are downloaded before a service worker can
--- lib/core/route/router_navigator.dart
+++ lib/core/route/router_navigator.dart
@@ -11,63 +11,67 @@ import 'route_name.dart';
 
 class RootNavigator {
   final GoRouter makeRoutes = GoRouter(
-    initialLocation: "/",
-    routes: [
-      // Route pour l'écran d'initialisation
-      GoRoute(
-        name: welcome_view,
-        path: '/',
-        builder: (context, state) => const InitializationPage(),
-      ),
-      GoRoute(
-              name: '/',
-              path: '/:tgWebAppData',
-              builder: (BuildContext context, GoRouterState state)  => const InitializationPage()),
-      GoRoute(
-        name: "telegram",
-        path: '/telegram',
-        builder: (context, state) {
-          // Extraire les paramètres de l'URL
-          final queryParams = state.extra as Map<String, String>;
-          return TelegramWebAppPage(queryParams: queryParams);
-        },
-      ),
-      // Route pour l'écran de walkthrough
-      // GoRoute(
-      //   name: splash_view,
-      //   path: '/splash',
-      //   builder: (context, state) => Walkthrough(),
-      // ),
+      initialLocation: "/",
+      routes: [
+        // Route pour l'écran d'initialisation
+        GoRoute(
+          name: welcome_view,
+          path: '/',
+          builder: (context, state) => const InitializationPage(),
+        ),
+        GoRoute(
+            name: '/',
+            path: '/:tgWebAppData',
+            builder: (BuildContext context, GoRouterState state) =>
+                const InitializationPage()),
+        GoRoute(
+          name: "telegram",
+          path: '/telegram',
+          builder: (context, state) {
+            // Extraire les paramètres de l'URL
+            final queryParams = state.extra as Map<String, String>;
+            return TelegramWebAppPage(queryParams: queryParams);
+          },
+        ),
+        // Route pour l'écran de walkthrough
+        // GoRoute(
+        //   name: splash_view,
+        //   path: '/splash',
+        //   builder: (context, state) => Walkthrough(),
+        // ),
 
-      GoRoute(
-        name: home_view,
-        path: '/home_vr',
-        // Toutes les routes pour le client commencent par /client/
-        builder: (BuildContext context, GoRouterState state) => HomeVrScreen(),
-        routes: <RouteBase>[
-          GoRoute(
-            name: amis_view,
-            path: 'amis',
-            builder: (context, state) => const AmisPage(),
-          ),
-          GoRoute(
-            name: revenu_view,
-            path: 'revenu',
-            builder: (context, state) => const AllRevenusPage(),
-          ),
-          GoRoute(
-            name: qg_view,
-            path: 'qg',
-            builder: (context, state) => QGScreen(),
-          ),
-        ],
-      ),
-    ], // redirect to the login page if the user is not logged in
-    // redirect: (BuildContext context, GoRouterState state) {
-    //   // if the user is not logged in, they need to login
+        GoRoute(
+          name: home_view,
+          path: '/home_vr',
+          // Toutes les routes pour le client commencent par /client/
+          builder: (BuildContext context, GoRouterState state) =>
+              HomeVrScreen(),
+          routes: <RouteBase>[
+            GoRoute(
+              name: amis_view,
+              path: 'amis',
+              builder: (context, state) => const AmisPage(),
+            ),
+            GoRoute(
+              name: revenu_view,
+              path: 'revenu',
+              builder: (context, state) => const AllRevenusPage(),
+            ),
+            GoRoute(
+              name: qg_view,
+              path: 'qg',
+              builder: (context, state) => QGScreen(),
+            ),
+          ],
+        ),
+      ],
+      errorBuilder: (_, GoRouterState state) =>
+          InitializationPage() // redirect to the login page if the user is not logged in
+      // redirect: (BuildContext context, GoRouterState state) {
+      //   // if the user is not logged in, they need to login
 
-    //   return '/';
-    // },
-  );
+      //   return '/';
+      // },
+      );
   // Routes pour l'application livreur
 }
