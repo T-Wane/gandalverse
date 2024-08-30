a3502ce148e3d48a5ca546e632413a14f77a4c32
Jbriguel <dossouyovojoseph04@gmail.com>
2024-08-30 16:04:15 +0000

    update - new

--- lib/di/global_dependencies.config.dart
+++ lib/di/global_dependencies.config.dart
@@ -10,21 +10,23 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:gandalverse/core/providers/charge_provider.dart' as _i7;
 import 'package:gandalverse/core/providers/user_provider.dart' as _i10;
+import 'package:gandalverse/core/repositories/dailyRewardRepository.dart'
+    as _i12;
 import 'package:gandalverse/core/repositories/tabAndEarnRepository.dart'
     as _i11;
 import 'package:gandalverse/core/repositories/user_repository.dart' as _i9;
 import 'package:gandalverse/core/services/explorer_service/explorer_service.dart'
     as _i8;
 import 'package:gandalverse/core/services/QG_services/equipe_service.dart'
-    as _i12;
-import 'package:gandalverse/core/services/QG_services/partenaire_service.dart'
     as _i13;
+import 'package:gandalverse/core/services/QG_services/partenaire_service.dart'
+    as _i14;
 import 'package:gandalverse/data/firebase_client.dart' as _i6;
 import 'package:gandalverse/data/shared_preferences/shared_preferences_client.dart'
     as _i3;
 import 'package:gandalverse/data/telegram_client.dart' as _i5;
 import 'package:gandalverse/data/tg_storage/telegram_cloudStorage.dart' as _i4;
-import 'package:gandalverse/di/global_dependencies_module.dart' as _i14;
+import 'package:gandalverse/di/global_dependencies_module.dart' as _i15;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
@@ -66,12 +68,14 @@ extension GetItInjectableX on _i1.GetIt {
         ));
     gh.singleton<_i11.TapAndEarnRepository>(
         () => _i11.TapAndEarnRepository(gh<_i10.UserProvider>()));
-    gh.lazySingleton<_i12.EquipeService>(
-        () => _i12.EquipeService(gh<_i10.UserProvider>()));
-    gh.lazySingleton<_i13.PartenaireService>(
-        () => _i13.PartenaireService(gh<_i10.UserProvider>()));
+    gh.singleton<_i12.DailyRewardManager>(
+        () => _i12.DailyRewardManager(gh<_i10.UserProvider>()));
+    gh.lazySingleton<_i13.EquipeService>(
+        () => _i13.EquipeService(gh<_i10.UserProvider>()));
+    gh.lazySingleton<_i14.PartenaireService>(
+        () => _i14.PartenaireService(gh<_i10.UserProvider>()));
     return this;
   }
 }
 
-class _$GlobalDependenciesModule extends _i14.GlobalDependenciesModule {}
+class _$GlobalDependenciesModule extends _i15.GlobalDependenciesModule {}
