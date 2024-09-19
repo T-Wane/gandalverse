5383e9262048a72a58d7d30d43897035c39041d6
Jbriguel <dossouyovojoseph04@gmail.com>
2024-09-19 20:10:46 +0000

    update +++

--- lib/screens/defis/components/_build_inviter_firends_card.dart
+++ lib/screens/defis/components/_build_inviter_firends_card.dart
@@ -1,107 +1,165 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:gandalverse/components/default_btn.dart';
+import 'package:gandalverse/core/providers/user_provider.dart';
 import 'package:gandalverse/screens/amis/components/bottom_invite_btns.dart';
 import 'package:gandalverse/screens/defis/components/annonceCard.dart';
 import 'package:gandalverse/themes/color/themeColors.dart';
 import 'package:gandalverse/themes/images/appImages.dart';
 import 'package:gandalverse/widgets/bottomSheet_cardContent.dart';
 import 'package:gandalverse/widgets/customImageView.dart';
+import 'package:gandalverse/widgets/reward/reward_animation.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class BuildInviterFirends extends StatelessWidget {
+class BuildInviterFirends extends StatefulWidget {
   const BuildInviterFirends({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
-    return AnnonceCard(
-        // title: 'Youtube',
-        text: 'Inviter 3 Amis \n',
-        reward: "20 000",
-        imagePath: Images.invites3Amis,
-        backColors: const [
-          Colors.white,
-          Colors.white,
-        ],
-        fit: BoxFit.contain,
+  State<BuildInviterFirends> createState() => _BuildInviterFirendsState();
+}
+
+class _BuildInviterFirendsState extends State<BuildInviterFirends> {
+  String task_inviterThreeFriendsKey = "task_inviter_3_amis_KEY";
+  late SharedPreferences prefs;
+
+  void claimed() async {
+    prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(task_inviterThreeFriendsKey, true);
+  }
+
+  void init() async {
+    prefs = await SharedPreferences.getInstance();
+    setState(() {});
+  }
+
+  bool isClaimed() {
+    return prefs.getBool(task_inviterThreeFriendsKey) ?? false;
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      init();
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  Widget _buildClaimedBtn(int nFriends) {
+    if (nFriends >= 3 && !isClaimed()) {
+      return DefaultButton(
+        backColor: Colors.purple.shade300,
+        text: 'Recupérer',
+        elevation: 1.0,
+        textColor: Colors.white,
+        fontSize: 13,
+        height: 40,
         press: () {
-          CardContentBottomSheet.show(context,
-              child: Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      "Inviter vos amis",
-                      maxLines: 1,
-                      textAlign: TextAlign.center,
-                      textDirection: TextDirection.ltr,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(
-                          fontSize: 15,
-                          color: Themecolors.Color3,
-                          fontFamily: "Aller",
-                          fontWeight: FontWeight.normal),
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    AutoSizeText(
-                      'Invitez 3 amis et obtenez un bonus exceptionnel !',
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context).textTheme.labelSmall!.copyWith(
-                            color: Themecolors.Color3.withOpacity(0.95),
-                            fontWeight: FontWeight.normal,
-                          ),
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        CustomImageView(
-                          imagePath: Images.coin_dollar,
-                          fit: BoxFit.contain,
-                          height: 40,
-                          width: 40,
-                        ),
-                        AutoSizeText(
-                          '20 000',
-                          maxLines: 1,
-                          presetFontSizes: [22, 20, 18, 15, 14],
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Themecolors.Color3,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    DefaultButton(
-                      backColor: Colors.grey.shade300,
-                      text: 'Vérifier',
-                      elevation: 1.0,
-                      textColor: Colors.grey.shade800,
-                      fontSize: 13,
-                      height: 40,
-                      press: () {},
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    const bottomInviteBtns(),
-                  ],
-                ),
-              ),
-              fit: BoxFit.contain,
-              image: Images.invites3Amis);
+          RewardAnimation.show(context);
+          claimed();
+          Future.delayed(const Duration(seconds: 3), () {
+            RewardAnimation.hide(context);
+          });
+          Navigator.of(context).pop();
         },
-        textColor: Colors.black,
-        titleColor: Themecolors.Color3);
+      );
+    }
+    return const SizedBox.shrink();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<UserProvider>(builder: (context, _userProvider, child) {
+      return AnnonceCard(
+          // title: 'Youtube',
+          text: 'Inviter 3 Amis \n',
+          reward: "20 000",
+          imagePath: Images.invites3Amis,
+          isComplete: isClaimed(),
+          backColors: const [
+            Colors.white,
+            Colors.white,
+          ],
+          fit: BoxFit.contain,
+          press: () {
+            CardContentBottomSheet.show(context,
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "Inviter vos amis",
+                        maxLines: 1,
+                        textAlign: TextAlign.center,
+                        textDirection: TextDirection.ltr,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                            fontSize: 15,
+                            color: Themecolors.Color3,
+                            fontFamily: "Aller",
+                            fontWeight: FontWeight.normal),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      AutoSizeText(
+                        'Invitez 3 amis et obtenez un bonus exceptionnel !',
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.labelSmall!.copyWith(
+                              color: Themecolors.Color3.withOpacity(0.95),
+                              fontWeight: FontWeight.normal,
+                            ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CustomImageView(
+                            imagePath: Images.coin_dollar,
+                            fit: BoxFit.contain,
+                            height: 40,
+                            width: 40,
+                          ),
+                          AutoSizeText(
+                            '20 000',
+                            maxLines: 1,
+                            presetFontSizes: [22, 20, 18, 15, 14],
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Themecolors.Color3,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      _buildClaimedBtn(_userProvider.friends.length),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      const bottomInviteBtns(),
+                    ],
+                  ),
+                ),
+                fit: BoxFit.contain,
+                image: Images.invites3Amis);
+          },
+          textColor: Colors.black,
+          titleColor: Themecolors.Color3);
+    });
   }
 }
